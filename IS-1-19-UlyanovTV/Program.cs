d71df836a67a6851329a45d03aa51a8f163dbe87
Ульянов Тимофей <ulyanovt@list.ru>
2021-12-05 13:06:05 +0500

    Задание 3

--- IS-1-19-UlyanovTV/Program.cs
+++ IS-1-19-UlyanovTV/Program.cs
@@ -19,4 +19,35 @@ namespace IS_1_19_UlyanovTV
             Application.Run(new Menu());
         }
     }
+
+    //класс для подключени
+    public class ConnectDB
+    {
+        //поля, к которым будут присваиваться значения, для подключения к Базе данных
+        public string conn;
+        public string Host;
+        public string Port;
+        public string User;
+        public string Database;
+        public string Password;
+
+        //метод Инициализации (создания подключения к бд)
+        public string Initialization()
+        {
+            //Присваиваем значения переменным
+            Host = "caseum.ru";
+            Port = "33333";
+            User = "test_user";
+            Database = "db_test";
+            Password = "test_pass";
+            //объявляем поле отвечающее за создание подключения к бд
+            string connecionString;
+            //создаём подключение
+            connecionString = $"server={Host};port={Port};user={User};database={Database};password={Password};";
+
+            conn = connecionString;
+            //возвращаем созданное подлючение (для удобства в виде переменной conn)
+            return conn;
+        }
+    }
 }
---
+++ IS-1-19-UlyanovTV/Zadanie3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace IS_1_19_UlyanovTV
+{
+    public partial class Zadanie3 : Form
+    {
+        public Zadanie3()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //создаём экземпляр класса
+            ConnectDB ConnDb = new ConnectDB();
+            //создаём экзепляр класса MySqlConnection и присваиваем ему значение которое возвращает метод Initialization
+            MySqlConnection connDb = new MySqlConnection(ConnDb.conn);
+            //объявляем переменную запроса к БД
+            string zapros = "SELECT id, fio, theme_kurs FROM t_stud";
+            try
+            {
+                connDb.Open();
+                //Создаём экземпляр класса MySqlDataAdapter, и даём ему 2 переменные (запрос к бд, и сами данные для подключения)
+                MySqlDataAdapter adapter = new MySqlDataAdapter(zapros, connDb);
+                //Воображаемая таблица с данными
+                DataSet dataset = new DataSet();
+                //Объединяем 2 переменные успеха, в результате чего Адаптер вытягивает данные в Воображаемую таблицу
+                adapter.Fill(dataset);
+                dataGridView1.DataSource = dataset.Tables[0];
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка! Что-то пошло не так!");
+            }
+            finally
+            {
+                connDb.Close();
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //Штука, которая будет действовать когда кликнешь ЛКМ в dataGridView
+            if (!e.RowIndex.Equals(-1) && !e.ColumnIndex.Equals(-1) && e.Button.Equals(MouseButtons.Left))
+            {
+                dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+                dataGridView1.CurrentRow.Selected = true;
+
+                string index;
+                string id;
+
+                index = dataGridView1.SelectedCells[0].RowIndex.ToString();
+                id = dataGridView1.Rows[Convert.ToInt32(index)].Cells[1].Value.ToString();
+                MessageBox.Show(id);
+            }
+        }
+    }
+}
